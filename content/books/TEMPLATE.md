d167b1aa2a7b145c3968c792831f7f69e245cddc
Michael Neeley <micneeley14@gmail.com>
2022-07-14 11:21:27 -0400

    adds new post templates (#5)
    * adds new post templates
    
    * changed template names
    
    * change titles

---
+++ content/books/TEMPLATE.md
@@ -0,0 +1,7 @@
+Title: BOOK
+Date: 2020-11-19
+Status: draft
+BookAuthor: XXX
+Rating: 3
+
+CONTENT
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,15 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Michael Neeley'
-SITENAME = 'neeley.dev'
-SITEURL = ''
+AUTHOR = "Michael Neeley"
+SITENAME = "neeley.dev"
+SITEURL = ""
 
-PATH = 'content'
+PATH = "content"
 
-TIMEZONE = 'America/New_York'
+TIMEZONE = "America/New_York"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -21,24 +21,24 @@ AUTHOR_FEED_RSS = None
 
 # Blogroll
 LINKS = (
-    ('Pelican', 'http://getpelican.com/'),
-    ('Python.org', 'http://python.org/'),
-    ('Jinja2', 'http://jinja.pocoo.org/'),
-    ('You can modify those links in your config file', '#'),
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+    ("You can modify those links in your config file", "#"),
 )
 
 # Social widget
 SOCIAL = (
-    ('You can add links in your config file', '#'),
-    ('Another social link', '#'),
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
 
-THEME = './themes/michael'
+THEME = "./themes/michael"
 
 # TEMPLATE_PAGES = {"pages/about.html": "about.html"}
 
